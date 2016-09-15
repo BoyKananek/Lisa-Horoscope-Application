a171577d86b0e23be18ba5e8e46ff958bb3938fd
boyKananek <kananek.ati@hotmail.com>
2016-09-15 16:27:35 +0700

    update version of the package in ionic via npm

--- platforms/android/assets/www/js/app.js
+++ platforms/android/assets/www/js/app.js
@@ -23,17 +23,22 @@ angular.module('App', ["ionic","ngCordovaOauth"])
     });
   $urlRouterProvider.otherwise('/home');
 })
-.controller('NavCtrl',function($scope,$location,$state,$http,$templateCache){
+
+
+.controller('NavCtrl',function($scope,$location,$state,$http,$templateCache,$ionicPopup){
   var fbLoginSuccess = function (userData) {
-      facebookConnectPlugin.api("/me?fields=id,gender,email,first_name,last_name",["email"],
+      //facebookConnectPlugin.api(userData['authResponse']['userID']+"/?fields=id,email,first_name,last_name",["email"],
+      facebookConnectPlugin.api("me/?fields=id,email,first_name,last_name",["email"],
       function (result) {
-          alert("Login Successful");
+          var alertPopup = $ionicPopup.alert({
+             title: 'Login Successful',
+             template: 'Hello '+ result["first_name"] +" "+result["last_name"]
+          });
           $state.go('profile');
-          //alert("Result: " + JSON.stringify(result));
-          $scope.id = result.id;
-          $scope.token = result.token;
-          $scope.email = result.email;
-          $scope.name = result.first_name +" "+result.last_name;
+          $scope.id = result["id"];
+          $scope.token = result["token"];
+          $scope.email = result["email"];
+          $scope.name = result["first_name"] +" "+result["last_name"];
           var dataObj = {
             id: $scope.id,
             token:$scope.token,
@@ -42,11 +47,12 @@ angular.module('App', ["ionic","ngCordovaOauth"])
           };
           var res = $http.post('https://lisahoroscope.herokuapp.com/api/users',dataObj);
           res.success(function(data,status,header,config){
-            //alert("Login Successful");
+            //alert("Save users name Successful");
           });
           res.error(function(data,status,headers,config){
              //alert("failure message:" + JSON.stringify({data:data}));
           });
+
           // saving to databases
           $scope.id ='';
           $scope.token ='';
@@ -62,7 +68,13 @@ angular.module('App', ["ionic","ngCordovaOauth"])
   $scope.loginFacebook = function(){
     facebookConnectPlugin.login(["public_profile","email"],
         fbLoginSuccess,
-        function (error) { alert("" + error) }
+        function (error) {
+          var alertPopup = $ionicPopup.alert({
+             title: 'Login failure',
+             template: 'Please type again'
+          });
+
+        }
     );
   }
 })
@@ -78,46 +90,87 @@ angular.module('App', ["ionic","ngCordovaOauth"])
 
 })
 
-.controller('profileCtrl',function($scope,$state,$http,$templateCache,BDService,dateFilter){
+
+.controller('profileCtrl',function($scope,$state,$http,$templateCache,$ionicPopup,BDService,dateFilter){
+
+  $scope.logoutFacebook = function(){
+    facebookConnectPlugin.logout(function(result){
+      //alert("logout");
+      var alertPopup = $ionicPopup.alert({
+         title: 'Bye Bye',
+         template: 'Logout from application'
+      });
+      $state.go('home');
+    },function(err){
+      //alert("logout err: "+err);
+      var alertPopup = $ionicPopup.alert({
+         title: 'Alert',
+         template: 'Logout failure, please try again'
+      });
+    });
+  }
   $scope.submitData = function(){
-    $state.go('horoscope');
-    $scope.month = parseInt(dateFilter($scope.birthdate,'MM'),10);
-    $scope.day = parseInt(dateFilter($scope.birthdate,'dd'),10);
-    if($scope.month == 12 && $scope.day >= 22 || $scope.month == 1 && $scope.day == 1){
-      BDService.save("Capricorn");
+    var inputText = document.getElementById("dateInput").value;
+    if (inputText == ""){
+      //alert("Please enter your birth date....");
+      var alertPopup = $ionicPopup.alert({
+         title: 'Alert',
+         template: 'Please enter your birth date .....'
+      });
     }
-    else if ($scope.month == 1 && $scope.day >= 20 || $scope.month == 2 && $scope.day <= 18){
-      BDService.save("Aquarius");
-    }
-    else if ($scope.month == 2 && $scope.day >= 19 || $scope.month == 3 && $scope.day <= 20){
-      BDService.save("Pisces");
-    }
-    else if ($scope.month == 3 && $scope.day >= 21 || $scope.month == 4 && $scope.day <= 19){
-      BDService.save("Aries");
-    }
-    else if ($scope.month == 4 && $scope.day >= 20 || $scope.month == 5 && $scope.day <= 20){
-      BDService.save("Taurus");
-    }
-    else if ($scope.month == 5 && $scope.day >= 21 || $scope.month == 6 && $scope.day <= 21){
-      BDService.save("Gemini");
-    }
-    else if ($scope.month == 6 && $scope.day >= 22 || $scope.month == 7 && $scope.day <= 22){
-      BDService.save("Cancer");
-    }
-    else if ($scope.month == 7 && $scope.day >= 23 || $scope.month == 8 && $scope.day <= 22){
-      BDService.save("Leo");
-    }
-    else if ($scope.month == 8 && $scope.day >= 23 || $scope.month == 9 && $scope.day <= 22){
-      BDService.save("Virgo");
-    }
-    else if ($scope.month == 9 && $scope.day >= 23 || $scope.month == 10 && $scope.day <= 22){
-      BDService.save("Libra");
-    }
-    else if ($scope.month == 10 && $scope.day >= 23 || $scope.month == 11 && $scope.day <= 21){
-      BDService.save("Scorpio");
-    }
-    else if ($scope.month == 11 && $scope.day >= 22 || $scope.month == 12 && $scope.day <= 21){
-      BDService.save("Sagittarius");
+    else{
+      $state.go('horoscope');
+      $scope.month = parseInt(dateFilter($scope.birthdate,'MM'),10);
+      $scope.day = parseInt(dateFilter($scope.birthdate,'dd'),10);
+      if($scope.month == 12 && $scope.day >= 22 || $scope.month == 1 && $scope.day == 1){
+        BDService.save("Capricorn");
+
+      }
+      else if ($scope.month == 1 && $scope.day >= 20 || $scope.month == 2 && $scope.day <= 18){
+        BDService.save("Aquarius");
+
+      }
+      else if ($scope.month == 2 && $scope.day >= 19 || $scope.month == 3 && $scope.day <= 20){
+        BDService.save("Pisces");
+
+      }
+      else if ($scope.month == 3 && $scope.day >= 21 || $scope.month == 4 && $scope.day <= 19){
+        BDService.save("Aries");
+
+      }
+      else if ($scope.month == 4 && $scope.day >= 20 || $scope.month == 5 && $scope.day <= 20){
+        BDService.save("Taurus");
+
+      }
+      else if ($scope.month == 5 && $scope.day >= 21 || $scope.month == 6 && $scope.day <= 21){
+        BDService.save("Gemini");
+
+      }
+      else if ($scope.month == 6 && $scope.day >= 22 || $scope.month == 7 && $scope.day <= 22){
+        BDService.save("Cancer");
+
+      }
+      else if ($scope.month == 7 && $scope.day >= 23 || $scope.month == 8 && $scope.day <= 22){
+        BDService.save("Leo");
+
+      }
+      else if ($scope.month == 8 && $scope.day >= 23 || $scope.month == 9 && $scope.day <= 22){
+        BDService.save("Virgo");
+
+      }
+      else if ($scope.month == 9 && $scope.day >= 23 || $scope.month == 10 && $scope.day <= 22){
+        BDService.save("Libra");
+
+      }
+      else if ($scope.month == 10 && $scope.day >= 23 || $scope.month == 11 && $scope.day <= 21){
+        BDService.save("Scorpio");
+
+      }
+      else if ($scope.month == 11 && $scope.day >= 22 || $scope.month == 12 && $scope.day <= 21){
+        BDService.save("Sagittarius");
+
+      }
+
     }
     //BDService.save($scope.day,$scope.month);
     //condition find sign of zodiac and query to api
@@ -125,7 +178,7 @@ angular.module('App', ["ionic","ngCordovaOauth"])
   }
 })
 
-.controller('horoCtrl',function($scope,$state,$http,$templateCache,BDService){
+.controller('horoCtrl',function($scope,$state,$http,$templateCache,BDService,$ionicPopup){
   $scope.zodiac = BDService.get();
   $http.get("https://lisahoroscope.herokuapp.com/api/horoscope/"+$scope.zodiac).then(function(response){
     $scope.daily = response.data.Daily_Horoscope;
@@ -136,6 +189,34 @@ angular.module('App', ["ionic","ngCordovaOauth"])
     $scope.wellness = response.data.Wellness;
     $scope.icon = response.data.Icon;
   });
+  $scope.shareFB = function(){
+    facebookConnectPlugin.getLoginStatus(
+        function (status) {
+            //alert("current status: " + JSON.stringify(status));
+            facebookConnectPlugin.showDialog(
+            {
+                method: "feed",
+                picture:'http://www.lisaguru.com',
+                name:'Lisa Horoscope',
+                message: $scope.zodiac,
+                caption: 'Lets\' see your horoscope ',
+                description: 'https://lisahoroscope.herokuapp.com/view/horoscope/'+$scope.zodiac
+            },
+            function (response) {
+              //alert(JSON.stringify(response))
+              //alert("Post!");
+              var alertPopup = $ionicPopup.alert({
+                 title: 'Successful',
+                 template: 'Post! on Facebook already.'
+              });
+            },
+            function (response) {
+              //alert(JSON.stringify(response))
+            }
+          );
+        }
+    );
+  }
 })
 
 .run(function($ionicPlatform) {
